0fc26522a4f33fcdc38d66f047f1bdbfd2f3678c
Regina Kapralova <r.kapralova@g.nsu.ru>
2022-10-13 09:59:17 +0700

    Update Hall.cs

--- Lab1/Hall.cs
+++ Lab1/Hall.cs
@@ -5,51 +5,51 @@ namespace PickyBrideProblem
 {
 	static class Hall
 	{
-		private static readonly Dictionary<int, string> _contenders = new Dictionary<int, string>();
-		private static Contender _contender;
+        private static readonly Dictionary<int, string> _contenders = new Dictionary<int, string>();
+        private static Contender _contender;
 
-		public static void Start()
-		{
-			String line;
-			StreamReader sr = new StreamReader("OneHundredUniqueNames.txt");
-			for (int i = 1; i <= 100; i++)
-			{
-				line = sr.ReadLine();
-				_contenders.Add(i, line);
-			}
-			sr.Close();
-		}
+        public static void Start()
+        {
+            String line;
+            StreamReader sr = new StreamReader("OneHundredUniqueNames.txt");
+            for (int i = 1; i <= 100; i++)
+            {
+                line = sr.ReadLine();
+                _contenders.Add(i, line);
+            }
+            sr.Close();
+        }
 
-		public static IContender InviteContender()
-		{
-			if (_contenders.Count <= 0) return null;
-			Random rnd = new Random();
-			while (true)
-			{
-				int value = rnd.Next(1, 101);
-				if (_contenders.ContainsKey(value))
-				{
-					_contender = new Contender(_contenders[value], value);
-					Console.WriteLine(_contenders[value]);
-					IContender c = _contender;
-					_contenders.Remove(value);
-					return c;
-				}
-				else
-				{
-					continue;
-				}
-			}
-		}
+        public static IContender InviteContender()
+        {
+            if (_contenders.Count <= 0) return null;
+            Random rnd = new Random();
+            while (true)
+            {
+                int value = rnd.Next(1, 101);
+                if (_contenders.ContainsKey(value))
+                {
+                    _contender = new Contender(_contenders[value], value);
+                    Console.WriteLine(_contenders[value]);
+                    IContender c = _contender;
+                    _contenders.Remove(value);
+                    return c;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
 
-		public static void Get_married()
-		{
-			Console.WriteLine((_contender.Mark > 50) ? _contender.Mark : 0);
-		}
+        public static void Get_married()
+        {
+            Console.WriteLine((_contender.Mark > 50) ? _contender.Mark : 0);
+        }
 
-		public static void Dont_get_married()
-		{
-			Console.WriteLine(10);
-		}
-	}
+        public static void Dont_get_married()
+        {
+            Console.WriteLine(10);
+        }
+    }
 }
